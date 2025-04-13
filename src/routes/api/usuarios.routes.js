610a7c82abbe5a4ab8fd7be756c1e96f1c7ab487
Cora <enero.noventaytres@gmail.com>
2025-04-13 21:43:54 +0200

    ruta de registro

--- src/routes/api/usuarios.routes.js
+++ src/routes/api/usuarios.routes.js
@@ -8,7 +8,7 @@ const {
 } = require("../../controllers/usuarios.controller");
 const { checkUsuarioId } = require("../../middleware/usuario.middleware");
 
-router.post("/", register);
+router.post("/register", register);
 router.post("/login", login);
 router.put("/:usuarioId", checkUsuarioId, updateUser);
 router.delete("/:usuarioId", remove);
