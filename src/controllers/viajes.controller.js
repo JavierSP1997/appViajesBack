ac8517c9be7b0a69ca9387c44cdbbe82a65baf89
Laura Moreno <chipifsdev@gmail.com>
2025-04-14 17:52:43 +0200

    borrados los console.log

--- src/controllers/viajes.controller.js
+++ src/controllers/viajes.controller.js
@@ -1,7 +1,6 @@
 const tripModel = require("../models/viajes.model");
 
 const getAllTrips = async (req, res, next) => {
-    console.log("pasa por el controlador");
     try {
         const trips = await tripModel.selectAll();
         res.json(trips);
--- src/models/viajes.model.js
+++ src/models/viajes.model.js
@@ -1,7 +1,6 @@
 const db = require("../config/db.config");
 
 const selectAll = async () => {
-    console.log("pasa por el modelo");
     const [result] = await db.query("select * from viajes");
     return result;
 };
