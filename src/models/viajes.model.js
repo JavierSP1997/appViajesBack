d3c3f43bb45f8f7ad1fcde73500efd926985cb9e
Gian Caorlin <caorlingiancarlo@gmail.com>
2025-04-14 17:41:00 +0200

    creación de peticiones para viajes

--- src/models/viajes.model.js
+++ src/models/viajes.model.js
@@ -1,9 +1,77 @@
 const db = require("../config/db.config");
 
-const selectAll = async () => {
-    console.log("pasa por el modelo");
-    const [result] = await db.query("select * from viajes");
-    return result;
+const insert = async ({
+  nombre_viaje,
+  fecha_inicio,
+  fecha_fin,
+  coste_por_persona,
+  personas_minimas,
+  localizacion,
+  itinerario,
+  imagen,
+}) => {
+  const [result] = await db.query(
+    "INSERT INTO viajes (nombre_viaje, fecha_inicio, fecha_fin, coste_por_persona, personas_minimas, localizacion, itinerario, imagen) VALUES (?, ?, ?, ?, ?, ?, ?, ?)",
+    [
+      nombre_viaje,
+      fecha_inicio,
+      fecha_fin,
+      coste_por_persona,
+      personas_minimas,
+      localizacion,
+      itinerario,
+      imagen,
+    ]
+  );
+  return result;
 };
 
-module.exports = { selectAll };
+const selectAll = async () => {
+  console.log("pasa por el modelo");
+  const [result] = await db.query("select * from viajes");
+  return result;
+};
+
+const selectById = async (viajeId) => {
+  const [result] = await db.query("SELECT * FROM viajes WHERE id_viaje = ?", [
+    viajeId,
+  ]);
+  return result[0];
+};
+
+const updateById = async ({
+  nombre_viaje,
+  fecha_inicio,
+  fecha_fin,
+  coste_por_persona,
+  personas_minimas,
+  localizacion,
+  itinerario,
+  imagen,
+  id_viaje,
+}) => {
+  const [result] = await db.query(
+    "UPDATE viajes SET nombre_viaje = ?, fecha_inicio = ?, fecha_fin = ?, coste_por_persona = ?, personas_minimas = ?, localizacion = ?, itinerario = ?, imagen = ? WHERE id_viaje = ?",
+    [
+      nombre_viaje,
+      fecha_inicio,
+      fecha_fin,
+      coste_por_persona,
+      personas_minimas,
+      localizacion,
+      itinerario,
+      imagen,
+      id_viaje,
+    ]
+  );
+  return result.affectedRows > 0;
+};
+
+const deleteById = async (viajeId) => {
+  const [result] = await db.query("DELETE FROM viajes WHERE id_viaje = ?", [
+    viajeId,
+  ]);
+  return result.affectedRows > 0;
+};
+
+module.exports = { insert, selectAll, selectById, updateById, deleteById };
