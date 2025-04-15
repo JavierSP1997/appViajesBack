1aa0a7226ea5175ccc7e125bbf4396afcf5fb97f
Gian Caorlin <caorlingiancarlo@gmail.com>
2025-04-15 18:11:48 +0200

    coment de funcionamiento del helper

--- src/helpers/utils.js
+++ src/helpers/utils.js
@@ -2,11 +2,11 @@
 const jwt = require("jsonwebtoken"); // npm install jsonwebtoken - Genera y verifica tokens JWT para autenticación.
 
 const createToken = (usuario) => {
-    const obj = {
-        id: usuario.id_usuario,
-    };
-    return jwt.sign(obj, "fideos fritos");
-    //alimentamos con el payload(el objeto creado con la data de usuario necesaria, y un string como filtro codificador)
+  const obj = {
+    id: usuario.id_usuario, // En este caso, solo estamos guardando el id del usuario.
+  };
+  return jwt.sign(obj, "fideos fritos");
+  //alimentamos con el payload(el objeto creado con la data de usuario necesaria, y un string como filtro codificador)
 };
 
 module.exports = { createToken };
