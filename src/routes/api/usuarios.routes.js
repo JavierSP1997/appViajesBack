19de0f3783c22aabbe6631c4e6664c0a73d12ca2
Laura Moreno <chipifsdev@gmail.com>
2025-04-15 15:39:52 +0200

    añadido el checktoken a las rutas y modificada la función checktoken id: usuario.id_usuario

--- src/routes/api/usuarios.routes.js
+++ src/routes/api/usuarios.routes.js
@@ -6,15 +6,14 @@ const {
     updateUser,
     remove,
     getOne,
-    getAll,
 } = require("../../controllers/usuarios.controller");
+const { checkToken } = require("../../middleware/auth.middlewares");
 const { checkUsuarioId } = require("../../middleware/usuario.middleware");
 
-router.get("/", getAll);
+router.get("/", checkToken, getOne);
 router.post("/register", register);
 router.post("/login", login);
 router.put("/:usuarioId", checkUsuarioId, updateUser);
 router.delete("/:usuarioId", remove);
-router.get("/:usuarioId", getOne);
 
 module.exports = router;
