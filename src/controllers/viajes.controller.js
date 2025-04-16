5ff4bbc141b949fba0b6d2017dca7b11fb818284
CHIPILAPTOP\laura <chipifsdevp@gmail.com>
2025-04-16 18:01:49 +0200

    cambios de identacion

--- src/controllers/viajes.controller.js
+++ src/controllers/viajes.controller.js
@@ -1,85 +1,72 @@
 const viajesModel = require("../models/viajes.model");
 
 const getAllViajes = async (req, res, next) => {
-  try {
-    const viajes = await viajesModel.selectAll();
-    res.json(viajes);
-  } catch (error) {
-    next(error);
-  }
+    try {
+        const viajes = await viajesModel.selectAll();
+        res.json(viajes);
+    } catch (error) {
+        next(error);
+    }
 };
 
 const getViajeById = async (req, res, next) => {
-  try {
-    const viajeId = req.params.viajeId;
-
-    const viajeResult = await viajesModel.selectById(viajeId);
-    const viaje = viajeResult[0];
-
-    if (!viaje) {
-      return res.status(404).json({ message: "Viaje no encontrado" });
+    try {
+        const viaje = await viajesModel.selectById(req.params.viajeId);
+        if (viaje) {
+            res.json(viaje);
+        } else {
+            res.status(404).json({ message: "Viaje no encontrado" });
+        }
+    } catch (error) {
+        next(error);
     }
-
-    const participantes = await viajesModel.selectParticipantesByViajeId(viajeId);
-
-    res.json({
-      ...viaje,
-      participantes,
-    });
-
-  } catch (error) {
-    next(error);
-  }
 };
 
-
 const registerViaje = async (req, res, next) => {
-  try {
-    console.log(req.usuario)
-    req.body.usuarios_id_usuario = req.usuario.id_usuario;
-    const newViaje = await viajesModel.insert(req.body);
-    res.status(201).json({
-      message: "Viaje registrado con éxito",
-      id: newViaje.insertId,
-    });
-  } catch (error) {
-    next(error);
-  }
+    try {
+        const newViaje = await viajesModel.insert(req.body);
+        res.status(201).json({
+            message: "Viaje registrado con éxito",
+            id: newViaje.insertId,
+        });
+    } catch (error) {
+        next(error);
+    }
 };
 
 const updateViaje = async (req, res, next) => {
-  try {
-    const updated = await viajesModel.updateById({
-      id: req.params.id,
-      ...req.body,
-    });
-    if (updated) {
-      res.json({ message: "Viaje actualizado con éxito" });
-    } else {
-      res.status(404).json({ message: "Viaje no encontrado" });
+    try {
+        const updated = await viajesModel.updateById({
+            id: req.params.id,
+            ...req.body,
+        });
+        if (updated) {
+            res.json({ message: "Viaje actualizado con éxito" });
+        } else {
+            res.status(404).json({ message: "Viaje no encontrado" });
+        }
+    } catch (error) {
+        next(error);
     }
-  } catch (error) {
-    next(error);
-  }
 };
 
 const removeViaje = async (req, res, next) => {
-  try {
-    const deleted = await viajesModel.deleteById(req.params.id);
-    if (deleted) {
-      res.json({ message: "Viaje eliminado con éxito" });
-    } else {
-      res.status(404).json({ message: "Viaje no encontrado" });
+    try {
+        const deleted = await viajesModel.deleteById(req.params.id);
+        if (deleted) {
+            res.json({ message: "Viaje eliminado con éxito" });
+        } else {
+            res.status(404).json({ message: "Viaje no encontrado" });
+        }
+    } catch (error) {
+        next(error);
     }
-  } catch (error) {
-    next(error);
-  }
 };
 
 module.exports = {
-  getAllViajes,
-  getViajeById,
-  registerViaje,
-  updateViaje,
-  removeViaje,
+    getAllViajes,
+    getViajeById,
+    registerViaje,
+    updateViaje,
+    removeViaje,
 };
