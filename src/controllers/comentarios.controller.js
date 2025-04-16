89d181da6296bb424741237bd8cb44642034f0f7
Javier Sancho <sanchopej@gmail.com>
2025-04-16 13:48:02 +0200

    Peticiones para obtener los comentarios de un viaje y de poder agregar comentarios creado

--- src/controllers/comentarios.controller.js
+++ src/controllers/comentarios.controller.js
@@ -1,2 +1,34 @@
-const comentariosModel = require("../models/comentarios.model");
-module.exports = {};
+const Comentario = require("../models/comentarios.model");
+
+const obtenerComentarios = async (req, res) => {
+    try {
+        const { viajeId } = req.params;
+        const comentarios = await Comentario.getByViajeId(viajeId);
+        res.json(comentarios);
+    } catch (error) {
+        res.status(500).json({
+            message: "Error al obtener los comentarios",
+            error,
+        });
+    }
+};
+
+const agregarComentario = async (req, res) => {
+    try {
+        const { viajeId } = req.params;
+        const { usuario_id, comentario } = req.body;
+        const insertId = await Comentario.createComment(
+            viajeId,
+            usuario_id,
+            comentario,
+        );
+        res.status(201).json({ message: "Comentario agregado", id: insertId });
+    } catch (error) {
+        res.status(500).json({ message: "Error al agregar comentario", error });
+    }
+};
+
+module.exports = {
+    obtenerComentarios,
+    agregarComentario,
+};
